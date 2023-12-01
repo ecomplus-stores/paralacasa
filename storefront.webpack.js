b82117f20bfded9014e8411b739628d2889f9d03
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-12-01 00:40:15 -0300

    chore: edit storefront webpack to get alias from two files

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js')
+      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/js/ShippingCalculator.js'),
       './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html')
     }
   }
